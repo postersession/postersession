18296b41323330c5483426aac0503e4970d09da9
Jonathan <jbinas@gmail.com>
2019-05-13 21:11:12 -0400

    rephrased email

--- posters/views.py
+++ posters/views.py
@@ -27,60 +27,59 @@ def upload(request, access_key):
     poster = get_object_or_404(Poster, access_key=access_key)
 
     if request.method == 'POST':
-        msg = EmailMultiAlternatives(
+        log_msg = EmailMultiAlternatives(
             subject="Poster: {title}".format(title=poster.title),
             body="Uploaded",
             from_email="postersession.ai <submissions@mg.postersession.ai>",
             to=["log <log@mg.postersession.ai>"])
-        msg.send()
-        
+        log_msg.send()
+
         form = PDFForm(request.POST, request.FILES, instance=poster)
         if form.is_valid():
-            msg = EmailMultiAlternatives(
+            log_msg = EmailMultiAlternatives(
                 subject="Poster: {title}".format(title=poster.title),
                 body="Valid file",
                 from_email="postersession.ai <submissions@mg.postersession.ai>",
                 to=["log <log@mg.postersession.ai>"])
-            msg.send()
-        
+            log_msg.send()
+
             form.save()
             try:
                 poster.generate_preview()
             except:
                 logger.exception('Exception while converting PDF')
-                #TODO notify admins about this case
                 poster.active = False
                 poster.save()
                 messages.warning(request,
                     'Your poster was uploaded successfully, but we had trouble converting it. We will look into it and activate your poster within the next few hours.')
 
-                msg = EmailMultiAlternatives(
+                log_msg = EmailMultiAlternatives(
                     subject="Poster: {title}".format(title=poster.title),
                     body="Conversion failed",
                     from_email="postersession.ai <submissions@mg.postersession.ai>",
                     to=["log <log@mg.postersession.ai>"])
-                msg.send()
-        
+                log_msg.send()
+
                 return redirect('detail', slug=poster.slug)
             poster.active = True
             poster.save()
             messages.success(request, 'Your poster was uploaded successfully!')
 
-            msg = EmailMultiAlternatives(
+            log_msg = EmailMultiAlternatives(
                 subject="Poster: {title}".format(title=poster.title),
                 body="Conversion successful",
                 from_email="postersession.ai <submissions@mg.postersession.ai>",
                 to=["log <log@mg.postersession.ai>"])
-            msg.send()
+            log_msg.send()
 
             return redirect('detail', slug=poster.slug)
         else:
-            msg = EmailMultiAlternatives(
+            log_msg = EmailMultiAlternatives(
                 subject="Poster: {title}".format(title=poster.title),
                 body="Invalid file",
                 from_email="postersession.ai <submissions@mg.postersession.ai>",
                 to=["log <log@mg.postersession.ai>"])
-            msg.send()
+            log_msg.send()
 
     else:
         form = PDFForm(instance=poster)
--- send_emails.py
+++ send_emails.py
@@ -26,22 +26,25 @@ def send_email(paper):
     body = u"""
 Hello {author_names},
 
-We're collecting poster PDFs from ICLR'19 to make them public and help people who weren't able to attend the conference.
+You presented the paper "{title}" at a poster session at ICLR'19.
+Poster sessions are a great way for researchers to gain a quick overview of the current state of their field. It would be great if this was available even to people who were not able to attend the conference.
 
-We hope to get all the ICLR posters available online, creating a "virtual poster session". 
+We hope to get all the ICLR posters available online, creating a "virtual poster session".
 
-We just need a minute of your time -- please visit this link, where you can upload a PDF of your poster for your paper, "{title}"
+We just need a minute of your time -- please visit this link, where you can upload a PDF of your poster:
+
 https://postersession.ai/upload/{upload_key}
 
 To reduce load, we are only emailing the first three authors of each paper.
 
 All posters will be made public on https://postersession.ai/
 
-Thanks,
+Please reach out if you have any questions.
+
+Thanks a lot,
 Jonathan Binas (Mila) and Avital Oliver (Google Brain).
 
 to={to}
-
 
 """.format(
         author_names=u", ".join([author["name"] for author in filtered_authors]),
@@ -50,7 +53,7 @@ to={to}
         title=title,
         to=["{name} <{email}>".format(name=author['name'], email=author['email']) for author in filtered_authors])
 
-    
+
 
     msg = EmailMultiAlternatives(
         subject="Share your ICLR poster on postersession.ai",
@@ -78,5 +81,5 @@ def send_emails():
         papers = json.load(iclr_json)
         for paper in papers[0:5]:
             send_email(paper)
-        
+
 
