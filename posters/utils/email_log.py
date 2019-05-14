732122ae07d83c0e62d817a41488cf45bf997b95
Jonathan <jbinas@gmail.com>
2019-05-14 17:48:33 -0400

    more informative log messages

---
+++ posters/utils/email_log.py
@@ -0,0 +1,31 @@
+
+from django.core.mail import EmailMultiAlternatives
+from django.urls import reverse
+
+import posters.views
+
+
+class LogEmail:
+    def __init__(self, poster):
+        self.subject = "Poster: {title}".format(title=poster.title),
+        self.from_email = "postersession.ai <submissions@mg.postersession.ai>"
+        self.to_email = ["log <log@mg.postersession.ai>"]
+        self.messages = [
+            'title: %s' % poster.title,
+            'authors: %s' % ', '.join([str(a) for a in poster.author_list()]),
+            'poster id: %s' % poster.pk,
+            'upload url: %s' % reverse(posters.views.upload, args=[poster.access_key]),
+            '',
+        ]
+
+    def add_message(self, msg):
+        self.messages.append(msg)
+
+    def send(self):
+        EmailMultiAlternatives(
+            subject=self.subject,
+            body='\n'.join(self.messages),
+            from_email=self.from_email,
+            to=self.to_email
+        ).send()
+
--- posters/views.py
+++ posters/views.py
@@ -1,40 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib import messages
-from django.core.mail import EmailMultiAlternatives
 import time
-
 import logging
-logger = logging.getLogger(__name__)
 
 from .models import Poster
 from .forms import PDFForm
+from .utils import email_log
 
+logger = logging.getLogger(__name__)
 
 
 USR_CONVERSION_FAILED = "Your poster was uploaded successfully, but we had trouble converting it. We will look into it and activate your poster within the next few hours."
 USR_SUCCESS = "Your poster was uploaded successfully!"
 USR_INVALID_FILE = "Please upload a valid file."
 USR_EXISTING_FILE = "Your poster has been uploaded already. You may update it by uploading a new file."
-
-
-class LogEmail:
-    def __init__(self, poster):
-        self.subject = "Poster: {title}".format(title=poster.title),
-        self.from_email = "postersession.ai <submissions@mg.postersession.ai>"
-        self.to_email = ["log <log@mg.postersession.ai>"]
-        self.messages = []
-
-    def add_message(self, msg):
-        self.messages.append(msg)
-
-    def send(self):
-        EmailMultiAlternatives(
-            subject=self.subject,
-            body=', '.join(self.messages),
-            from_email=self.from_email,
-            to=self.to_email
-        ).send()
 
 
 #TODO: change to generic views
@@ -50,18 +30,18 @@ def detail(request, slug):
 
 def upload(request, access_key):
     poster = get_object_or_404(Poster, access_key=access_key)
-    log_email = LogEmail(poster)
+    log_email = email_log.LogEmail(poster)
     form = PDFForm(instance=poster)
     if poster.pdf:
         messages.info(request, USR_EXISTING_FILE)
 
     if request.method == 'POST':
         try:
-            log_email.add_message('uploaded')
+            log_email.add_message('INFO: uploaded')
 
             form = PDFForm(request.POST, request.FILES, instance=poster)
             if form.is_valid():
-                log_email.add_message('valid file')
+                log_email.add_message('INFO: valid file')
                 form.save()
                 try:
                     poster.generate_preview()
@@ -69,20 +49,20 @@ def upload(request, access_key):
                     poster.active = False
                     poster.save()
 
-                    logger.exception('Exception while converting PDF')
+                    logger.exception('ERR: failed to convert PDF (id %s)' % poster.pk)
                     messages.warning(request, USR_CONVERSION_FAILED)
-                    log_email.add_message('conversion failed')
+                    log_email.add_message('ERR: conversion failed')
 
                     return redirect('detail', slug=poster.slug)
 
                 poster.active = True
                 poster.save()
                 messages.success(request, USR_SUCCESS)
-                log_email.add_message('conversion successful')
+                log_email.add_message('INFO: conversion successful')
 
                 return redirect('detail', slug=poster.slug)
             else:
-                log_email.add_message('invalid file')
+                log_email.add_message('ERR: invalid file')
                 messages.error(request, USR_INVALID_FILE)
         finally:
             log_email.send()
