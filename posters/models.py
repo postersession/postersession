60fba5b4c227197cc85c3006023d4f5d807d5dff
Jonathan <jbinas@gmail.com>
2019-05-14 12:16:42 -0400

    removing uniqueness constraint from author email

--- posters/models.py
+++ posters/models.py
@@ -20,7 +20,7 @@ class Conference(models.Model):
 
 class Author(models.Model):
     name = models.CharField(max_length=256)
-    email = models.EmailField(unique=True, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.name + ' <' + str(self.email) + '>'
